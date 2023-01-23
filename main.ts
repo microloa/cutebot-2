b89608e658f4d559b7f96b10017a900e8da4cd8b
microloa <97388308+microloa@users.noreply.github.com>
2023-01-22 19:28:57 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+cuteBot.forward()
+cuteBot.stopcar()
 basic.forever(function () {
-	
+    if (input.lightLevel() < 10) {
+        cuteBot.singleheadlights(cuteBot.RGBLights.RGB_R, 125, 125, 125)
+        cuteBot.singleheadlights(cuteBot.RGBLights.RGB_L, 125, 125, 125)
+    } else {
+        cuteBot.singleheadlights(cuteBot.RGBLights.RGB_R, 0, 0, 0)
+        cuteBot.singleheadlights(cuteBot.RGBLights.RGB_L, 0, 0, 0)
+    }
+    basic.pause(1000)
 })
